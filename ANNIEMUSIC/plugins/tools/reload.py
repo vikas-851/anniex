14f9c8cf139c5381f52170c6b12e16877e741d2c
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-08-11 15:58:07 +0530

    Update reload.py

--- ANNIEMUSIC/plugins/tools/reload.py
+++ ANNIEMUSIC/plugins/tools/reload.py
@@ -105,7 +105,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(1841914911)
+    & filters.user(7453770651)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
